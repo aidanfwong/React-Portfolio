ae2fdfd195f8b962892d2d664f4fb50c9ebacae8
aidanfwong <aidanfwong@gmail.com>
2024-06-28 23:06:23 -0400

    Final change for now: remove underline for hyperlinks

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -11,9 +11,9 @@ function About() {
         I enjoy tackling challenges; whether it's 
        coding or something completely new, I am always ready! 
        Feel free to connect with me on 
-       <a href="https://linkedin.com/in/aidanfwong" target="_blank"> LinkedIn</a> or
+       <a className={styles.links} href="https://linkedin.com/in/aidanfwong" target="_blank"> LinkedIn</a> or
         check out my projects on
-         <a href="https://github.com/aidanfwong" target="_blank"> GitHub</a>. 
+         <a className={styles.link} href="https://github.com/aidanfwong" target="_blank"> GitHub</a>. 
         Let's create something amazing together!
         </p>
         </span>
